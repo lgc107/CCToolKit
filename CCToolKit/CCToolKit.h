ac45402b1cfe555c3f124646f49c332e0d1837e5
Harry_L <15645969688@163.com>
2018-05-14 16:49:43 +0800

    Initial commit

---
+++ CCToolKit/CCToolKit.h
@@ -0,0 +1,19 @@
+//
+//  CCToolKit.h
+//  CCToolKit
+//
+//  Created by Harry_L on 2018/5/14.
+//  Copyright © 2018年 Harry_L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CCToolKit.
+FOUNDATION_EXPORT double CCToolKitVersionNumber;
+
+//! Project version string for CCToolKit.
+FOUNDATION_EXPORT const unsigned char CCToolKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CCToolKit/PublicHeader.h>
+
+
