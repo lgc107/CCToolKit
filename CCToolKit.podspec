0f9334c10fece76ece19c6661b5700b60ff03ee5
Harry_L <15645969688@163.com>
2018-05-26 16:44:29 +0800

    modify pod -> 0.0.4

--- CCToolKit.podspec
+++ CCToolKit.podspec
@@ -91,8 +91,8 @@ Pod::Spec.new do |s|
   #  Not including the public_header_files will make all headers public.
   #
 
-  s.source_files  = "CCToolKit/*"
-  s.exclude_files = "CCToolKit/Exclude"
+  s.source_files  = "CCToolKit/**/*.{h,m}"
+  #s.exclude_files = "CCToolKit/Exclude"
 
   # s.public_header_files = "Classes/**/*.h"
 
--- CCToolKit/CCToolKit.h
+++ CCToolKit/CCToolKit.h
@@ -16,7 +16,7 @@ FOUNDATION_EXPORT const unsigned char CCToolKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CCToolKit/PublicHeader.h>
 
-
+#import <CCToolKit/CCSoftwareManager.h>
 #import <CCToolKit/CCFileManager.h>
 #import <CCToolKit/CCVersion.h>
 #import <CCToolKit/UIImage+Extensions.h>
