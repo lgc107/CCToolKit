43630d3d8bf5c73a8b37649996890407fdcc3e74
Harry_L <15645969688@163.com>
2018-06-12 17:05:51 +0800

    【0.0.7】FileManager 新增方法

--- CCToolKit/CCStorage/CCFileManager.h
+++ CCToolKit/CCStorage/CCFileManager.h
@@ -17,47 +17,321 @@ typedef enum : NSUInteger {
 } DircetoryType;
 
 @interface CCFileManager : NSObject
-NS_ASSUME_NONNULL_BEGIN
+
 +(instancetype)shareManager;
 
+
+#pragma mark - path
+
+/**
+  沙盒的主目录路径
+ */
 @property (nonatomic,readonly) NSString *homePath;
 
+/**
+ 沙盒中Documents的目录路径
+ */
 @property (nonatomic,readonly) NSString *documentPath;
-
+/**
+ 沙盒中library的目录路径
+ */
 @property (nonatomic,readonly) NSString *libraryPath;
-
+/**
+ 沙盒中cache的目录路径
+ */
 @property (nonatomic,readonly) NSString *cachePath;
-
+/**
+ 沙盒中tmp的目录路径
+ */
 @property (nonatomic,readonly) NSString *tmpPath;
 
 
+/**
+ 获取文件完整路径
+
+ @param name 文件相对路径 eg: @"xxx.plist", @"xxx/xxx.plist"
+ @param type 沙盒指定的文件夹
+ @return 文件完整路径
+ */
 +(NSString *)pathForFileName:(NSString *)name inDircetoryType:(DircetoryType)type;
 
 
+/**
+ 根据文件完整路径获取上一级文件夹名称
 
-+(BOOL)createFileAtPath:(NSString *)fullPath;
+ @param path 文件完整路径
+ @return DirectoryName
+ */
++ (NSString *)directoryAtPath:(NSString *)path;
 
-+(BOOL)createFileAtPath:(NSString *)fullPath OverWrite:(BOOL)overwrite;
+/**
+  根据文件路径获取文件扩展类型
+ */
++ (NSString *)suffixAtPath:(NSString *)path;
 
-+(BOOL)createFileAtPath:(NSString *)fullPath OverWrite:(BOOL)overwrite Contents:(id)contents error:(NSError * _Nullable *_Nullable)error;
+#pragma mark - list Array
+/**
+ 遍历根目录
+ */
++ (NSArray *)cc_listFilesInHomeDirectoryByDeep:(BOOL)deep;
+/**
+ 遍历Library目录
+ */
++ (NSArray *)cc_listFilesInLibraryDirectoryByDeep:(BOOL)deep;
+/**
+ 遍历Document目录
+ */
++ (NSArray *)cc_listFilesInDocumentDirectoryByDeep:(BOOL)deep;
+/**
+ 遍历Tmp目录
+ */
++ (NSArray *)cc_listFilesInTmpDirectoryByDeep:(BOOL)deep;
 
-+(BOOL)createFileAtPath:(NSString *)fullPath IntermediateDirectories:(BOOL)intermediateDirectories attributes:(NSDictionary<NSFileAttributeKey, id> *_Nonnull)attributes error:(NSError * _Nullable *_Nullable)error;
+/**
+  遍历Caches目录
+ */
++ (NSArray *)cc_listFilesInCachesDirectoryByDeep:(BOOL)deep;
+/**
+ 文件遍历
+ 
+ @param path 目录的绝对路径
+ @param deep 是否深遍历 (1. 浅遍历：返回当前目录下的所有文件和文件夹；
+ 2. 深遍历：返回当前目录下及子目录下的所有文件和文件夹)
+ @return 遍历结果数组
+ */
++ (NSArray *)cc_listFilesInDirectoryAtPath:(NSString *)path deep:(BOOL)deep;
 
-+(BOOL)createFileAtPath:(NSString *)fullPath withIntermediateDirectories:(BOOL)intermediateDirectories OverWrite:(BOOL)overwrite attributes:(NSDictionary<NSFileAttributeKey, id> *_Nullable)attributes Contents:(id _Nullable )contents error:(NSError * _Nullable *_Nullable)error;
+#pragma mark - File Exist
 
-+(BOOL)fileExistsAtFullPath:(NSString *)fullPath;
+/**
+ 通过完整路径判断是否存在文件
+ */
++(BOOL)cc_fileExistsAtFullPath:(NSString *)fullPath;
 
-+(BOOL)fileExistsAtDocumentPath:(NSString *)fileName;
+/**
+  判断Document文件夹下是否含有fileName 文件
+ */
++(BOOL)cc_fileExistsAtDocumentPath:(NSString *)fileName;
 
-+(BOOL)removeItemAtPath:(NSString *)path;
+/**
+  判断路径是否为空(判空条件是文件大小为0，或者是文件夹下没有子文件)
+ */
++ (BOOL)cc_isEmptyItemAtPath:(NSString *)path error:(NSError *__autoreleasing *)error;
 
-+(BOOL)removeItemAtPath:(NSString *)path error:(NSError * _Nullable *_Nullable)error;
+#pragma mark - create
 
-+(BOOL)writeFileAtPath:(NSString *)path content:(id)content;
+/**
+  创建文件夹
+ */
++ (BOOL)cc_createDirectoryAtPath:(NSString *)path error:(NSError *__autoreleasing *)error;
 
-+(BOOL)writeFileAtPath:(NSString *)path content:(id)content error:(NSError **)error;
+
+/**
+ 在Document创建文件
+ */
++ (BOOL)cc_createFileAtDocumentDircetoryWithName:(NSString *)fileName;
+/**
+ 在Library创建文件
+ */
++ (BOOL)cc_createFileAtLibraryDircetoryWithName:(NSString *)fileName;
+/**
+ 在Cache创建文件
+ */
++ (BOOL)cc_createFileAtCacheDircetoryWithName:(NSString *)fileName;
+/**
+ 在Tmp创建文件
+ */
++ (BOOL)cc_createFileAtTmpDircetoryWithName:(NSString *)fileName;
+
+/**
+ 在指定路径创建文件
+ */
++ (BOOL)cc_createFileAtPath:(NSString *)path;
+/**
+ 在指定路径创建文件
+ */
++ (BOOL)cc_createFileAtPath:(NSString *)path error:(NSError *__autoreleasing *)error;
+
+/**
+  创建文件，是否覆盖
+ */
++ (BOOL)cc_createFileAtPath:(NSString *)path overwrite:(BOOL)overwrite;
+/**
+ 创建文件，包含文件内容
+ */
++ (BOOL)cc_createFileAtPath:(NSString *)path content:(id)content;
+/**
+ 创建文件，包含文件内容，是否覆盖
+ */
++ (BOOL)cc_createFileAtPath:(NSString *)path content:(id)content overwrite:(BOOL)overwrite;
+/*创建文件
+ *参数1：文件创建的路径
+ *参数2：写入文件的内容
+ *参数3：假如已经存在此文件是否覆盖
+ *参数4：错误信息
+ */
++ (BOOL)cc_createFileAtPath:(NSString *)path content:(id)content overwrite:(BOOL)overwrite error:(NSError *__autoreleasing *)error;
+
+#pragma mark - remove
+
+/**
+ 删除文件
+ */
++(BOOL)cc_removeItemAtPath:(NSString *)path;
+/**
+ 删除文件
+ */
++(BOOL)cc_removeItemAtPath:(NSString *)path error:(NSError * _Nullable *_Nullable)error;
 
 
-NS_ASSUME_NONNULL_END
+#pragma mark - write to file
+/**
+  写入文件内容
+ */
++(BOOL)cc_writeFileAtPath:(NSString *)path content:(id)content;
+/**
+ 写入文件内容
+ */
++(BOOL)cc_writeFileAtPath:(NSString *)path content:(id)content error:(NSError **)error;
+
+/**
+ 在文件末尾追加数据，！只建议用于文本追加
+ */
++(BOOL)cc_appendData:(NSData *)data withPath:(NSString *)path;
+
+#pragma mark - Read
++ (NSData *)cc_getFileData:(NSString *)filePath;
+
+
+#pragma mark - File Info
+#pragma mark -- file type
+/**
+判断目录是否是文件夹
+ */
++ (BOOL)cc_isDirectoryAtPath:(NSString *)path;
+
+/**
+ 判断目录是否是文件
+ */
++ (BOOL)cc_isFileAtPath:(NSString *)path;
+/**
+ 判断目录是否可以执行
+ */
++ (BOOL)cc_isExecutableItemAtPath:(NSString *)path;
+/**
+ 判断目录是否可读
+ */
++ (BOOL)cc_isReadableItemAtPath:(NSString *)path;
+/**
+  判断目录是否可写
+ */
++ (BOOL)cc_isWritableItemAtPath:(NSString *)path;
+
+#pragma mark -- date
+/**
+ 获取文件创建时间
+ */
++ (NSDate *)cc_creationDateOfItemAtPath:(NSString *)path;
+/**
+ 获取文件创建时间
+ */
++ (NSDate *)cc_creationDateOfItemAtPath:(NSString *)path error:(NSError *__autoreleasing *)error;
+
+/**
+ 获取文件修改时间
+ */
++ (NSDate *)cc_modificationDateOfItemAtPath:(NSString *)path;
+/**
+ 获取文件修改时间
+ */
++ (NSDate *)cc_modificationDateOfItemAtPath:(NSString *)path error:(NSError *__autoreleasing *)error ;
+
+#pragma mark -- directory size
+
+/**
+   获取文件夹大小
+ */
++ (NSNumber *)cc_sizeOfDirectoryAtPath:(NSString *)path;
+/**
+ 获取文件夹大小
+ */
++ (NSNumber *)cc_sizeOfDirectoryAtPath:(NSString *)path error:(NSError *__autoreleasing *)error;
+/**
+ 获取文件夹大小，返回格式化后的数值
+ */
++ (NSString *)cc_sizeFormattedOfDirectoryAtPath:(NSString *)path;
+/**
+ 获取文件夹大小，返回格式化后的数值(错误信息error)
+ */
++ (NSString *)cc_sizeFormattedOfDirectoryAtPath:(NSString *)path error:(NSError *__autoreleasing *)error;
+
+#pragma mark -- item size
+/**
+   获取目录大小
+ */
++ (NSNumber *)cc_sizeOfItemAtPath:(NSString *)path;
+
+/**
+  获取目录大小(错误信息error)
+ */
++ (NSNumber *)cc_sizeOfItemAtPath:(NSString *)path error:(NSError *__autoreleasing *)error;
+/**
+ 获取目录大小，返回格式化后的数值
+ */
++ (NSString *)cc_sizeFormattedOfItemAtPath:(NSString *)path;
+/**
+ 获取目录大小，返回格式化后的数值
+ */
++ (NSString *)cc_sizeFormattedOfItemAtPath:(NSString *)path error:(NSError *__autoreleasing *)error;
+
+#pragma mark -- file size
+/**
+ 获取文件大小
+ */
++ (NSNumber *)cc_sizeOfFileAtPath:(NSString *)path;
+/**
+ 获取文件大小
+ */
++ (NSNumber *)cc_sizeOfFileAtPath:(NSString *)path error:(NSError *__autoreleasing *)error;
+/**
+ 获取文件大小，返回格式化后的数值
+ */
++ (NSString *)cc_sizeFormattedOfFileAtPath:(NSString *)path;
+/**
+ 获取文件大小，返回格式化后的数值
+ */
++ (NSString *)cc_sizeFormattedOfFileAtPath:(NSString *)path error:(NSError *__autoreleasing *)error;
+
+#pragma mark - file attributes
+
+/**
+   根据key获取文件某个属性
+ */
++ (id)cc_attributeOfItemAtPath:(NSString *)path forKey:(NSString *)key;
+
+/**
+ 根据key获取文件某个属性
+ @param path 完整路径
+ @param key NSFileAttributeKey （NSFileManager中可找）
+ @param error 错误信息
+
+ */
++ (id)cc_attributeOfItemAtPath:(NSString *)path forKey:(NSString *)key error:(NSError *__autoreleasing *)error;
+
+/**
+  获取文件属性集合
+ */
++ (NSDictionary *)cc_attributesOfItemAtPath:(NSString *)path;
+
+/**
+ 获取文件属性集合
+
+ @param path 文件完整路径
+ @param error 错误信息
+ */
++ (NSDictionary *)cc_attributesOfItemAtPath:(NSString *)path error:(NSError *__autoreleasing *)error;
+
++ (NSString *)cc_sizeFormatted:(NSNumber *)size;
+
 @end
 
--- CCToolKit/CCStorage/CCFileManager.m
+++ CCToolKit/CCStorage/CCFileManager.m
@@ -17,6 +17,7 @@
     NSString *_tmpPath;
 }
 
+#pragma mark - shareInstance
 +(instancetype)shareManager{
     
     static CCFileManager *fileManager = nil;
@@ -28,6 +29,8 @@
     }
     return fileManager;
 }
+
+#pragma mark - path
 
 -(NSString *)homePath{
     if (!_homePath) {
@@ -93,71 +96,179 @@
     }
 }
 
-+(BOOL)createFileAtPath:(NSString *)fullPath{
-    return  [self createFileAtPath:fullPath withIntermediateDirectories:true OverWrite:false attributes:nil Contents:nil error:nil];
++ (NSString *)directoryAtPath:(NSString *)path {
+    return [path stringByDeletingLastPathComponent];
 }
 
-+(BOOL)createFileAtPath:(NSString *)fullPath OverWrite:(BOOL)overwrite {
-    return  [self createFileAtPath:fullPath withIntermediateDirectories:true OverWrite:overwrite attributes:nil Contents:nil error:nil];
++ (NSString *)suffixAtPath:(NSString *)path {
+    return [path pathExtension];
 }
 
-+(BOOL)createFileAtPath:(NSString *)fullPath OverWrite:(BOOL)overwrite Contents:(id)contents error:(NSError **)error{
-    return  [self createFileAtPath:fullPath withIntermediateDirectories:true OverWrite:overwrite attributes:nil Contents:contents error:error];
+#pragma mark - FileList
++ (NSArray *)cc_listFilesInHomeDirectoryByDeep:(BOOL)deep {
+    return [self cc_listFilesInDirectoryAtPath:[CCFileManager shareManager].homePath deep:deep];
 }
 
-+(BOOL)createFileAtPath:(NSString *)fullPath IntermediateDirectories:(BOOL)intermediateDirectories attributes:(NSDictionary<NSFileAttributeKey, id> *)attributes error:(NSError * _Nullable *)error{
-    return  [self createFileAtPath:fullPath withIntermediateDirectories:intermediateDirectories OverWrite:false attributes:attributes Contents:nil error:error];
++ (NSArray *)cc_listFilesInLibraryDirectoryByDeep:(BOOL)deep {
+    return [self cc_listFilesInDirectoryAtPath:[CCFileManager shareManager].libraryPath deep:deep];
 }
 
-+(BOOL)createFileAtPath:(NSString *)fullPath withIntermediateDirectories:(BOOL)intermediateDirectories OverWrite:(BOOL)overwrite attributes:(NSDictionary<NSFileAttributeKey, id> *)attributes Contents:(id)contents error:(NSError * _Nullable *)error{
-    if (![self fileExistsAtFullPath:fullPath]) {
-        BOOL created = [[NSFileManager defaultManager]createDirectoryAtPath:fullPath withIntermediateDirectories:true attributes:attributes error:error];
-        if (created && contents != nil) {
-            [self writeFileAtPath:fullPath content:contents error:error];
++ (NSArray *)cc_listFilesInDocumentDirectoryByDeep:(BOOL)deep {
+    return [self cc_listFilesInDirectoryAtPath:[CCFileManager shareManager].documentPath deep:deep];
+}
+
++ (NSArray *)cc_listFilesInTmpDirectoryByDeep:(BOOL)deep {
+    return [self cc_listFilesInDirectoryAtPath:[CCFileManager shareManager].tmpPath deep:deep];
+}
+
++ (NSArray *)cc_listFilesInCachesDirectoryByDeep:(BOOL)deep {
+    return [self cc_listFilesInDirectoryAtPath:[CCFileManager shareManager].cachePath deep:deep];
+}
+
++ (NSArray *)cc_listFilesInDirectoryAtPath:(NSString *)path deep:(BOOL)deep {
+    NSArray *listArr;
+    NSError *error;
+    NSFileManager *manager = [NSFileManager defaultManager];
+    if (deep) {
+        // 深遍历
+        NSArray *deepArr = [manager subpathsOfDirectoryAtPath:path error:&error];
+        if (!error) {
+            listArr = deepArr;
+        }else {
+            listArr = nil;
         }
-        return (created && [self isNotError:error]);
-    }
-    else if (overwrite && [self removeItemAtPath:fullPath error:error] && [self isNotError:error]){
-        BOOL created = [[NSFileManager defaultManager]createDirectoryAtPath:fullPath withIntermediateDirectories:true attributes:attributes error:error];
-        if (created && contents != nil) {
-            [self writeFileAtPath:fullPath content:contents error:error];
+    }else {
+        // 浅遍历
+        NSArray *shallowArr = [manager contentsOfDirectoryAtPath:path error:&error];
+        if (!error) {
+            listArr = shallowArr;
+        }else {
+            listArr = nil;
         }
-        return (created && [self isNotError:error]);
     }
-    return  false;
+    return listArr;
 }
 
-+(BOOL)fileExistsAtFullPath:(NSString *)fullPath{
+#pragma mark - 判断文件是否存在
++(BOOL)cc_fileExistsAtFullPath:(NSString *)fullPath{
     return [[NSFileManager defaultManager]fileExistsAtPath:fullPath];
 }
 
-+(BOOL)fileExistsAtDocumentPath:(NSString *)fileName{
++(BOOL)cc_fileExistsAtDocumentPath:(NSString *)fileName{
     return [[NSFileManager defaultManager]fileExistsAtPath:[self pathForFileName:fileName inDircetoryType:Document]];
 }
 
-+ (BOOL)removeItemAtPath:(NSString *)path {
-    return  [self removeItemAtPath:path error:nil];
+#pragma mark - 判断文件(夹)是否为空
++ (BOOL)cc_isEmptyItemAtPath:(NSString *)path error:(NSError *__autoreleasing *)error {
+    return ([self cc_isFileAtPath:path] &&
+            [[self cc_sizeOfItemAtPath:path error:error] intValue] == 0) ||
+    ([self cc_isDirectoryAtPath:path] &&
+     [[self cc_listFilesInDirectoryAtPath:path deep:NO] count] == 0);
 }
 
-+ (BOOL)removeItemAtPath:(NSString *)path error:(NSError * _Nullable *)error{
+
+
+#pragma mark - create
+
++ (BOOL)cc_createFileAtDocumentDircetoryWithName:(NSString *)fileName{
+     return [self cc_createFileAtPath:[self pathForFileName:fileName inDircetoryType:Document]];
+}
+
++(BOOL)cc_createFileAtLibraryDircetoryWithName:(NSString *)fileName{
+     return [self cc_createFileAtPath:[self pathForFileName:fileName inDircetoryType:Library]];
+}
+
+
++ (BOOL)cc_createFileAtCacheDircetoryWithName:(NSString *)fileName{
+    return [self cc_createFileAtPath:[self pathForFileName:fileName inDircetoryType:Caches]];
+}
+
++ (BOOL)cc_createFileAtTmpDircetoryWithName:(NSString *)fileName{
+    return [self cc_createFileAtPath:[self pathForFileName:fileName inDircetoryType:Tmp]];
+}
+
++ (BOOL)cc_createDirectoryAtPath:(NSString *)path error:(NSError *__autoreleasing *)error {
+    NSFileManager *manager = [NSFileManager defaultManager];
+    /* createDirectoryAtPath:withIntermediateDirectories:attributes:error:
+     * 参数1：创建的文件夹的路径
+     * 参数2：是否创建媒介的布尔值，一般为YES
+     * 参数3: 属性，没有就置为nil
+     * 参数4: 错误信息
+     */
+    BOOL isSuccess = [manager createDirectoryAtPath:path withIntermediateDirectories:YES attributes:nil error:error];
+    return isSuccess;
+}
+
++ (BOOL)cc_createFileAtPath:(NSString *)path {
+    return [self cc_createFileAtPath:path content:nil overwrite:YES error:nil];
+}
+
++ (BOOL)cc_createFileAtPath:(NSString *)path error:(NSError *__autoreleasing *)error {
+    return [self cc_createFileAtPath:path content:nil overwrite:YES error:error];
+}
+
++ (BOOL)cc_createFileAtPath:(NSString *)path overwrite:(BOOL)overwrite {
+    return [self cc_createFileAtPath:path content:nil overwrite:overwrite error:nil];
+}
+
++ (BOOL)cc_createFileAtPath:(NSString *)path content:(id)content {
+    return [self cc_createFileAtPath:path content:content overwrite:YES error:nil];
+}
+
++ (BOOL)cc_createFileAtPath:(NSString *)path content:(id)content overwrite:(BOOL)overwrite {
+    return [self cc_createFileAtPath:path content:content overwrite:overwrite error:nil];
+}
+
++ (BOOL)cc_createFileAtPath:(NSString *)path content:(id)content overwrite:(BOOL)overwrite error:(NSError *__autoreleasing *)error {
+    // 如果文件夹路径不存在，那么先创建文件夹
+    NSString *directoryPath = [self directoryAtPath:path];
+    if (![self cc_fileExistsAtFullPath:directoryPath]) {
+        // 创建文件夹
+        if (![self cc_createDirectoryAtPath:directoryPath error:error]) {
+            return NO;
+        }
+    }
+    // 如果文件存在，并不想覆盖，那么直接返回YES。
+    if (!overwrite) {
+        if ([self cc_fileExistsAtFullPath:path]) {
+            return YES;
+        }
+    }
+    /*创建文件
+     *参数1：创建文件的路径
+     *参数2：创建文件的内容（NSData类型）
+     *参数3：文件相关属性
+     */
+    BOOL isSuccess = [[NSFileManager defaultManager] createFileAtPath:path contents:nil attributes:nil];
+    if (content) {
+        [self cc_writeFileAtPath:path content:content error:error];
+    }
+    return isSuccess;
+}
+
+#pragma mark - remove
++ (BOOL)cc_removeItemAtPath:(NSString *)path {
+    return  [self cc_removeItemAtPath:path error:nil];
+}
+
++ (BOOL)cc_removeItemAtPath:(NSString *)path error:(NSError * _Nullable *)error{
     return [[NSFileManager defaultManager] removeItemAtPath:path error:error];
 }
 
-+(BOOL)writeFileAtPath:(NSString *)path content:(NSObject *)content
+#pragma mark - Write
++(BOOL)cc_writeFileAtPath:(NSString *)path content:(NSObject *)content
 {
-    return [self writeFileAtPath:path content:content error:nil];
+    return [self cc_writeFileAtPath:path content:content error:nil];
 }
 
-
-+(BOOL)writeFileAtPath:(NSString *)path content:(id)content error:(NSError **)error
++(BOOL)cc_writeFileAtPath:(NSString *)path content:(id)content error:(NSError **)error
 {
     if(content == nil)
     {
         [NSException raise:@"Invalid content" format:@"content can't be nil."];
     }
     
-    
-    [self createFileAtPath:path withIntermediateDirectories:true OverWrite:false attributes:nil Contents:nil error:error];
+    [self cc_createFileAtPath:path overwrite:false];
     
     
     if([content isKindOfClass:[NSMutableArray class]])
@@ -202,7 +313,7 @@
     }
     else if([content isKindOfClass:[UIImageView class]])
     {
-        return [self writeFileAtPath:path content:((UIImageView *)content).image error:error];
+        return [self cc_writeFileAtPath:path content:((UIImageView *)content).image error:error];
     }
     else if([content conformsToProtocol:@protocol(NSCoding)])
     {
@@ -217,6 +328,188 @@
     return YES;
 }
 
++ (BOOL)cc_appendData:(NSData *)data withPath:(NSString *)path
+{
+    BOOL result = [self cc_createFileAtPath:path];
+    if (result) {
+        NSFileHandle *handle = [NSFileHandle fileHandleForWritingAtPath:path];
+        [handle seekToEndOfFile];
+        [handle writeData:data];
+        [handle synchronizeFile];
+        [handle closeFile];
+        return YES;
+    } else {
+        NSLog(@"%s Failed",__FUNCTION__);
+        return NO;
+    }
+}
+
+#pragma mark - Read
++ (NSData *)cc_getFileData:(NSString *)filePath
+{
+    NSFileHandle *handle = [NSFileHandle fileHandleForReadingAtPath:filePath];
+    NSData *fileData = [handle readDataToEndOfFile];
+    [handle closeFile];
+    return fileData;
+}
+
+
+
+
+#pragma mark - File Info
+#pragma mark -- file type
++ (BOOL)cc_isDirectoryAtPath:(NSString *)path {
+    return ([self cc_attributeOfItemAtPath:path forKey:NSFileType error:nil] == NSFileTypeDirectory);
+}
+
++ (BOOL)cc_isFileAtPath:(NSString *)path {
+    return ([self cc_attributeOfItemAtPath:path forKey:NSFileType error:nil] == NSFileTypeRegular);
+}
+
++ (BOOL)cc_isExecutableItemAtPath:(NSString *)path {
+    return [[NSFileManager defaultManager] isExecutableFileAtPath:path];
+}
+
++ (BOOL)cc_isReadableItemAtPath:(NSString *)path {
+    return [[NSFileManager defaultManager] isReadableFileAtPath:path];
+}
+
++ (BOOL)cc_isWritableItemAtPath:(NSString *)path {
+    return [[NSFileManager defaultManager] isWritableFileAtPath:path];
+}
+
+
+#pragma mark -- create Date
+
++ (NSDate *)cc_creationDateOfItemAtPath:(NSString *)path {
+    return [self cc_creationDateOfItemAtPath:path error:nil];
+}
+//获取文件创建的时间
++ (NSDate *)cc_creationDateOfItemAtPath:(NSString *)path error:(NSError *__autoreleasing *)error {
+    return (NSDate *)[self cc_attributeOfItemAtPath:path forKey:NSFileCreationDate error:error];
+}
+
+#pragma mark -- modification Date
++ (NSDate *)cc_modificationDateOfItemAtPath:(NSString *)path {
+    return [self cc_modificationDateOfItemAtPath:path error:nil];
+}
+//获取文件修改的时间
++ (NSDate *)cc_modificationDateOfItemAtPath:(NSString *)path error:(NSError *__autoreleasing *)error {
+    return (NSDate *)[self cc_attributeOfItemAtPath:path forKey:NSFileModificationDate error:error];
+}
+
+#pragma mark --directory size
+
++ (NSNumber *)cc_sizeOfDirectoryAtPath:(NSString *)path {
+    return [self cc_sizeOfDirectoryAtPath:path error:nil];
+}
+
++ (NSNumber *)cc_sizeOfDirectoryAtPath:(NSString *)path error:(NSError *__autoreleasing *)error {
+    if ([self cc_isDirectoryAtPath:path]) {
+        //深遍历文件夹
+        NSArray *subPaths = [self cc_listFilesInDirectoryAtPath:path deep:YES];
+        NSEnumerator *contentsEnumurator = [subPaths objectEnumerator];
+        
+        NSString *file;
+        unsigned long long int folderSize = 0;
+        
+        while (file = [contentsEnumurator nextObject]) {
+            NSDictionary *fileAttributes = [[NSFileManager defaultManager] attributesOfItemAtPath:[path stringByAppendingPathComponent:file] error:nil];
+            folderSize += [[fileAttributes objectForKey:NSFileSize] intValue];
+        }
+        return [NSNumber numberWithUnsignedLongLong:folderSize];
+    }
+    return nil;
+}
+
++ (NSString *)cc_sizeFormattedOfDirectoryAtPath:(NSString *)path {
+    return [self cc_sizeFormattedOfDirectoryAtPath:path error:nil];
+}
+
++ (NSString *)cc_sizeFormattedOfDirectoryAtPath:(NSString *)path error:(NSError *__autoreleasing *)error {
+    NSNumber *size = [self cc_sizeOfDirectoryAtPath:path error:error];
+    if (size) {
+        return [self cc_sizeFormatted:size];
+    }
+    return nil;
+}
+
+
+#pragma mark --item size
++ (NSNumber *)cc_sizeOfItemAtPath:(NSString *)path {
+    return [self cc_sizeOfItemAtPath:path error:nil];
+}
+
++ (NSNumber *)cc_sizeOfItemAtPath:(NSString *)path error:(NSError *__autoreleasing *)error {
+    return (NSNumber *)[self cc_attributeOfItemAtPath:path forKey:NSFileSize error:error];
+}
+
++ (NSString *)cc_sizeFormattedOfItemAtPath:(NSString *)path {
+    return [self cc_sizeFormattedOfItemAtPath:path error:nil];
+}
+
++ (NSString *)cc_sizeFormattedOfItemAtPath:(NSString *)path error:(NSError *__autoreleasing *)error {
+    NSNumber *size = [self cc_sizeOfItemAtPath:path error:error];
+    if (size) {
+        return [self cc_sizeFormatted:size];
+    }
+    return nil;
+}
+
+
+#pragma mark --file size
++ (NSNumber *)cc_sizeOfFileAtPath:(NSString *)path {
+    return [self cc_sizeOfFileAtPath:path error:nil];
+}
+
++ (NSNumber *)cc_sizeOfFileAtPath:(NSString *)path error:(NSError *__autoreleasing *)error {
+    if ([self cc_isFileAtPath:path]) {
+        return [self cc_sizeOfItemAtPath:path error:error];
+    }
+    return nil;
+}
+
++ (NSString *)cc_sizeFormattedOfFileAtPath:(NSString *)path {
+    return [self cc_sizeFormattedOfFileAtPath:path error:nil];
+}
+
++ (NSString *)cc_sizeFormattedOfFileAtPath:(NSString *)path error:(NSError *__autoreleasing *)error {
+    NSNumber *size = [self cc_sizeOfFileAtPath:path error:error];
+    if (size) {
+        return [self cc_sizeFormatted:size];
+    }
+    return nil;
+}
+
+#pragma mark - file attributes
++ (id)cc_attributeOfItemAtPath:(NSString *)path forKey:(NSString *)key {
+    return [[self cc_attributesOfItemAtPath:path] objectForKey:key];
+}
+
++ (id)cc_attributeOfItemAtPath:(NSString *)path forKey:(NSString *)key error:(NSError *__autoreleasing *)error {
+    return [[self cc_attributesOfItemAtPath:path error:error] objectForKey:key];
+}
+
++ (NSDictionary *)cc_attributesOfItemAtPath:(NSString *)path {
+    return [self cc_attributesOfItemAtPath:path error:nil];
+}
+
++ (NSDictionary *)cc_attributesOfItemAtPath:(NSString *)path error:(NSError *__autoreleasing *)error {
+    return [[NSFileManager defaultManager] attributesOfItemAtPath:path error:error];
+}
+
+
+
+#pragma mark 将文件大小格式化为字节
++(NSString *)cc_sizeFormatted:(NSNumber *)size {
+    /*NSByteCountFormatterCountStyle枚举
+     *NSByteCountFormatterCountStyleFile 字节为单位，采用十进制的1000bytes = 1KB
+     *NSByteCountFormatterCountStyleMemory 字节为单位，采用二进制的1024bytes = 1KB
+     *NSByteCountFormatterCountStyleDecimal KB为单位，采用十进制的1000bytes = 1KB
+     *NSByteCountFormatterCountStyleBinary KB为单位，采用二进制的1024bytes = 1KB
+     */
+    return [NSByteCountFormatter stringFromByteCount:[size unsignedLongLongValue] countStyle:NSByteCountFormatterCountStyleFile];
+}
 
 +(BOOL)isNotError:(NSError **)error
 {
